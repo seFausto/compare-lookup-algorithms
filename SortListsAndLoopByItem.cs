24f83546eed6b3e48500f7a7a303746d40720a69
Fausto Ruvalcaba <se.fausto@outlook.com>
2024-08-27 11:49:26 -0700

    feat: add sorting algorithm

--- SortListsAndLoopByItem.cs
+++ SortListsAndLoopByItem.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CompareLookUpAlgorithms
 {
-    internal class SortListsAndLoopByItem
+    internal class SortListsAndLoopByItem(List<Guid> items, List<Guid> lookup)
     {
-        public SortListsAndLoopByItem(List<Guid> items, List<Guid> lookup)
+        public bool Process()
         {
-            
+            // sort both lists
+
+            items.Sort();
+            lookup.Sort();
+
+            //for loop to go throu each item
+            for (int itemCount = 0; itemCount < items.Count; itemCount++)
+            {
+                Console.WriteLine($"{items[itemCount]}, {lookup[itemCount]}");
+            }
+
+            return true;
         }
     }
 }
