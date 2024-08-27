95e75a9c49237a195c9a553b5a47661931cff502
Fausto Ruvalcaba <se.fausto@outlook.com>
2024-08-27 12:07:12 -0700

    feat: add setup and benchmarks

---
+++ SortListVsDictionaryLookup.cs
@@ -0,0 +1,59 @@
+﻿using BenchmarkDotNet.Attributes;
+using CompareLookUpAlgorithms;
+
+public class SortListVsDictionaryLookup
+{
+
+    private SortListsAndLoopByItem sortLists;
+    private UseDictionaryForLookup useDictionary;
+    public SortListVsDictionaryLookup()
+    {
+      
+    }
+
+
+    [GlobalSetup]
+    public void Setup() 
+    {
+        var items = new List<Guid>();
+        var lookup = new List<Guid>();
+
+        for (int i = 0; i < 1000; i++)
+        {
+            var guid = Guid.NewGuid();
+            items.Add(guid);
+            lookup.Add(guid);
+        }
+
+        items.Shuffle();
+        lookup.Shuffle();
+
+        sortLists = new SortListsAndLoopByItem(items, lookup);
+        useDictionary = new UseDictionaryForLookup(items, lookup);
+    }
+
+
+
+    [Benchmark]
+    public bool SortedLists() => sortLists.Process();
+
+    [Benchmark]
+    public bool UseDictionary() => useDictionary.Process();
+
+}
+
+public static class ListExtensions
+{
+    private static Random rng = new Random();
+
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            (list[n], list[k]) = (list[k], list[n]);
+        }
+    }
+}
