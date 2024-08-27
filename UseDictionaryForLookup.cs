af56a49ea119278a7a859ac382da3b06f7e54f45
Fausto Ruvalcaba <se.fausto@outlook.com>
2024-08-27 11:53:11 -0700

    feat: add dictionary lookup

---
+++ UseDictionaryForLookup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareLookUpAlgorithms
+{
+    internal class UseDictionaryForLookup(List<Guid> items, List<Guid> lookup)
+    {
+
+        public bool Process()
+        { 
+            var set = new HashSet<Guid>();
+
+            foreach (var item in lookup) 
+            {
+                set.Add(item);
+            }
+
+            foreach (var item in items) 
+            { 
+               Console.WriteLine($"{item} - {set.Contains(item)}");
+            }
+
+            return true;
+        }
+    }
+}
