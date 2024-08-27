74851f7f28954e74f439fa200a72bbd913813222
Fausto Ruvalcaba <se.fausto@outlook.com>
2024-08-27 11:43:57 -0700

    Add project files.

---
+++ SortListsAndLoopByItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareLookUpAlgorithms
+{
+    internal class SortListsAndLoopByItem
+    {
+        public SortListsAndLoopByItem(List<Guid> items, List<Guid> lookup)
+        {
+            
+        }
+    }
+}
